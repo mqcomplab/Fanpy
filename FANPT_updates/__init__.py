488f37d4508a6f13ae1a635dc73fabf6a31fc154
Rugwed Lokhande <101607593+rugwed-lokhande@users.noreply.github.com>
2024-04-22 14:52:08 -0400

    Add files via upload

---
+++ FANPT_updates/__init__.py
@@ -0,0 +1,18 @@
+r"""
+FanCI module.
+
+"""
+
+
+__all__ = [
+    "FanCI",
+    "APIG",
+    "AP1roG",
+    "DetRatio",
+]
+
+
+from .fanci import FanCI
+from .apig import APIG
+from .ap1rog import AP1roG
+from .detratio import DetRatio
