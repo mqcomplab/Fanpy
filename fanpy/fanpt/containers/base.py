9c94641e775038cd90392ceb088689f67b5db2ad
Carlos E. V. de Moura <demoura.phd@icloud.com>
2024-08-12 13:29:17 -0400

    Correct eqn import

--- fanpy/fanpt/containers/base.py
+++ fanpy/fanpt/containers/base.py
@@ -2,7 +2,7 @@ r"""Base class that contains the elements required to perform a FANPT calculatio
 
 from abc import ABCMeta, abstractmethod
 
-from fanpy.eqn import ProjectedSchrodinger
+from fanpy.eqn.projected import ProjectedSchrodinger
 import pyci
 
 
