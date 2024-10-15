8c95ffb5412e5edc9face0bd26fe69a6486d97e4
Carlos E. V. de Moura <demoura.phd@icloud.com>
2024-10-15 14:33:28 -0500

    Added printer to analysis tool
    Initial functions added to print sds in human-readable format.

---
+++ fanpy/analysis/printer.py
@@ -0,0 +1,77 @@
+r"""Collection of functions to print Slater determinants and related features.
+
+Functions
+---------
+print_sds(sds, nspatial) : {tuple, int}
+    Print Slater determinants from a wavefunction as lists of occupied (1) and unoccupied (0) MOs.
+sds_occ_indices(sds, nspatial) : {tuple, int}
+    Print Slater determinants from a wavefunction as lists of occupied MO indices.
+"""
+
+from fanpy.tools import slater
+
+
+def sds_occ(sds, nspatial):
+    """Print Slater determinants from a wavefunction as lists of occupied (1) and unoccupied (0) MOs.
+
+    Parameters
+    ----------
+    sds : tuple
+        A tuple containing Slater determinants.
+        It can be obtained from BaseWavefunction objects by the sds attribute.
+    nspatial : int
+        Number of spatial orbitals.
+
+    """
+    # Create list of alpha and beta spin indices as zero-padded binary strings
+    sds_str = [
+        [
+            format(slater.split_spin(sd, nspatial)[0], f"0{nspatial}b"),
+            format(slater.split_spin(sd, nspatial)[1], f"0{nspatial}b"),
+        ]
+        for sd in sds
+    ]
+    # Print header
+    print("Slater determinants represented by MO occupancies\n")
+    print(f"{'Alpha':<{nspatial}}  |  {'Beta':<{nspatial}}")
+    print("-" * (nspatial * 2 + 6))
+    # Print the Slater determinant strings with aligned formatting
+    for alpha, beta in sds_str:
+        print(f"{alpha:<{nspatial}}  |  {beta:<{nspatial}}")
+    print("-" * (nspatial * 2 + 6) + "\n")
+
+
+def sds_occ_indices(sds, nspatial):
+    """Print Slater determinants from a wavefunction as lists of occupied MO indices.
+
+    Parameters
+    ----------
+    sds : tuple
+        A tuple containing Slater determinants.
+        It can be obtained from BaseWavefunction objects by the sds attribute.
+    nspatial : int
+        Number of spatial orbitals.
+
+    """
+    # Create list of strings for alpha and beta spin indices
+    sds_str = [
+        [
+            " ".join(map(str, slater.occ_indices(slater.split_spin(sd, nspatial)[0]))),
+            " ".join(map(str, slater.occ_indices(slater.split_spin(sd, nspatial)[1]))),
+        ]
+        for sd in sds
+    ]
+
+    # Find the maximum lengths for formatting
+    alpha_len = max(len(sd_str[0]) for sd_str in sds_str)
+    beta_len = max(len(sd_str[1]) for sd_str in sds_str)
+
+    # Print header
+    print("Slater determinants represented by occupied MO indices\n")
+    print(f"{'Alpha':<{alpha_len}}  |  {'Beta':<{beta_len}}")
+    print("-" * (alpha_len + beta_len + 6))
+
+    # Print the Slater determinant strings with aligned formatting
+    for alpha, beta in sds_str:
+        print(f"{alpha:<{alpha_len}}  |  {beta:<{beta_len}}")
+    print("-" * (alpha_len + beta_len + 6) + "\n")
