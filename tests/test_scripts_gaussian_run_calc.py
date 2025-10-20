20438d7ead91dfb45dbc367a177d61c7605275d6
Krisztina Zsigmond <144738961+kzsigmond@users.noreply.github.com>
2025-10-20 12:07:23 -0400

    test: add simple tests for gaussian run calc

---
+++ tests/test_scripts_gaussian_run_calc.py
@@ -0,0 +1,24 @@
+from fanpy.scripts.gaussian.run_calc import run_calc
+from utils import find_datafile
+import subprocess
+
+def test_run_calc():
+    oneint = find_datafile("data/data_h2_hf_sto6g_oneint.npy")
+    twoint = find_datafile("data/data_h2_hf_sto6g_twoint.npy")
+    # attempt to run simple calculation with minimal inputs
+    run_calc(
+        nelec=2,
+        one_int_file=oneint,
+        two_int_file=twoint,
+        wfn_type="cisd")
+
+def test_run_calc_cli():
+    oneint = find_datafile("data/data_h2_hf_sto6g_oneint.npy")
+    twoint = find_datafile("data/data_h2_hf_sto6g_twoint.npy")
+    subprocess.check_output([
+        "fanpy_run_calc",
+        "--nelec", "2",
+        "--one_int_file", oneint,
+        "--two_int_file", twoint,
+        "--wfn_type", "cisd",
+    ])
