aba276c4b36c38024b98b368d79cf7e3008b508b
Krisztina Zsigmond <144738961+kzsigmond@users.noreply.github.com>
2025-11-18 17:29:30 -0500

    fix: memory chunks over s-space in interface module

--- fanpy/interface/fanci/legacy.py
+++ fanpy/interface/fanci/legacy.py
@@ -1660,16 +1660,16 @@ class ProjectedSchrodingerFanCI(ProjectedSchrodingerLegacyFanCI):
             isamp += 1
 
     def calculate_overlap_deriv_chunks(self):
-
-        tensor_mem = self.nactive * 8 / 1e6
+        size_sspace = self._sspace.shape[0]
+        tensor_mem = size_sspace * 8 / 1e6
         avail_mem = (self.max_memory - current_memory()) * 0.9
 
         chunk_size = max(1, math.floor(avail_mem / tensor_mem))
-        chunk_size = min(chunk_size, self.nactive)
+        chunk_size = min(chunk_size, size_sspace)
 
         chunks_list = []
-        for s_chunk in range(0, self.nactive, chunk_size):
-            f_chunk = min(self.nactive, s_chunk + chunk_size)
+        for s_chunk in range(0, size_sspace, chunk_size):
+            f_chunk = min(size_sspace, s_chunk + chunk_size)
             chunks_list.append([s_chunk, f_chunk])
 
         return chunks_list
