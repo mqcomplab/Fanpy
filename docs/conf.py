7a76c4f0ad84433f56dc49a05613ff743f24e8b6
Krisztina Zsigmond <144738961+kzsigmond@users.noreply.github.com>
2026-04-28 11:21:12 -0400

    fix: remove unused bibtex for references

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
-    "sphinxcontrib.bibtex",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
