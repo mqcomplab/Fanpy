c54c6ca61931817154a5eab1e4129a6e880d2e4c
Krisztina Zsigmond <144738961+kzsigmond@users.noreply.github.com>
2026-04-30 14:56:33 -0400

    chore: remove python wrapper

--- tests/test_wrapper_python_wrapper.py
+++ tests/test_wrapper_python_wrapper.py
@@ -3,13 +3,7 @@ import os
 import sys
 from subprocess import call
 
-from fanpy.tools.wrapper.python_wrapper import generate_hartreefock_results
-
 import numpy as np
-
-import pytest
-
-from utils import find_datafile
 
 
 def check_data_h2_rhf_sto6g(el_energy, nuc_nuc_energy, one_int, two_int):
@@ -124,68 +118,3 @@ def check_dependency(dependency):
     # python, but here we are.
     exit_code = call([python_name, "-c", "import {0}".format(dependency)])
     return exit_code == 0
-
-
-def test_generate_hartreefock_results_error():
-    """Test python_wrapper.generate_hartreefock_results.
-
-    Check if it raises correct error.
-
-    """
-    with pytest.raises(ValueError):
-        generate_hartreefock_results("sdf")
-    os.environ["HORTONPYTHON"] = "Asfasdfsadf"
-    with pytest.raises(FileNotFoundError):
-        generate_hartreefock_results("horton_hartreefock.py")
-
-
-@pytest.mark.skipif(
-    not check_dependency("horton"), reason="HORTON is not available or HORTONPATH is not set."
-)
-def test_horton_hartreefock_h2_rhf_sto6g():
-    """Test HORTON"s hartreefock against H2 HF STO-6G data from Gaussian."""
-    hf_data = generate_hartreefock_results(
-        "horton_hartreefock.py",
-        energies_name="energies.npy",
-        oneint_name="oneint.npy",
-        twoint_name="twoint.npy",
-        remove_npyfiles=True,
-        fn=find_datafile("data/data_h2.xyz"),
-        basis="sto-6g",
-        nelec=2,
-    )
-    check_data_h2_rhf_sto6g(*hf_data)
-
-
-@pytest.mark.skipif(
-    not check_dependency("horton"), reason="HORTON is not available or HORTONPATH is not set."
-)
-def test_horton_gaussian_fchk_h2_rhf_sto6g():
-    """Test HORTON"s gaussian_fchk against H2 HF STO-6G data from Gaussian."""
-    fchk_data = generate_hartreefock_results(
-        "horton_gaussian_fchk.py",
-        energies_name="energies.npy",
-        oneint_name="oneint.npy",
-        twoint_name="twoint.npy",
-        remove_npyfiles=True,
-        fchk_file=find_datafile("data/data_h2_hf_sto6g.fchk"),
-        horton_internal=False,
-    )
-    check_data_h2_rhf_sto6g(*fchk_data)
-
-
-@pytest.mark.skipif(
-    not check_dependency("horton"), reason="HORTON is not available or HORTONPATH is not set."
-)
-def test_gaussian_fchk_h2_uhf_sto6g():
-    """Test HORTON"s gaussian_fchk against H2 UHF STO-6G data from Gaussian."""
-    fchk_data = generate_hartreefock_results(
-        "horton_gaussian_fchk.py",
-        energies_name="energies.npy",
-        oneint_name="oneint.npy",
-        twoint_name="twoint.npy",
-        remove_npyfiles=True,
-        fchk_file=find_datafile("data/data_h2_uhf_sto6g.fchk"),
-        horton_internal=False,
-    )
-    check_data_h2_uhf_sto6g(*fchk_data)
