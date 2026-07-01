ac0a7448df7ce42697eb81aba4ff01ddabf19201
yohann-alcantara-masle <284711735+yohann-alcantara-masle@users.noreply.github.com>
2026-07-01 15:42:18 -0400

    fix: lccd_bis exops spin error

--- fanpy/corrections/lccd_bis.py
+++ fanpy/corrections/lccd_bis.py
@@ -76,9 +76,9 @@ class LCCD_bis:
 
 
     def _is_pair_excitation(self, i, j, a, b):
-        if i == j+self.nspatial and a == b+self.nspatial:
+        if i == self._get_opposite_spin(j) and a == self._get_opposite_spin(b):
             return True
-        if j == i+self.nspatial and b == a+self.nspatial:
+        if j == self._get_opposite_spin(i) and b == self._get_opposite_spin(a):
             return True
         return False
 
@@ -94,9 +94,9 @@ class LCCD_bis:
 
         for sub_exop in sub_exops:
             i, j, a, b = sub_exop
-            if i == j+self.nspatial and a == b+self.nspatial:
+            if i == self._get_opposite_spin(j) and a == self._get_opposite_spin(b):
                 sub_exops.remove(sub_exop)
-            if j == i+self.nspatial and b == a+self.nspatial:
+            if j == self._get_opposite_spin(i) and b == self._get_opposite_spin(a):
                 sub_exops.remove(sub_exop)
 
         return sub_exops
